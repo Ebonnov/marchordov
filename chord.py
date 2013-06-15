1e0ac0b77766febe3f9c1250603fce4a09823d65
ebonnov <ebonnov@113builder.com>
2013-06-14 23:10:53 -0600

    First commit to github

--- chord.py
+++ chord.py
@@ -1,10 +1,59 @@
 import random
-from flask import abort, Flask, jsonify, request, render_template
+from flask import abort, Flask, jsonify, request, render_template, session
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'it worked!'
+  return render_template('index.html')
+
+@app.route('/_randomsandbox')
+def random_sandbox():
+  base = 48 + random.randint(0, 11)
+  ority = random.randint(0, 1)
+  result = [base, base + 3 + ority, base + 7]
+  return jsonify(result=result)
+
+@app.route('/_generatefirstquestion')
+def first_question():
+  # 0: minor, 1: major, 2: dimished
+  answer = random.randint(0, 14) / 6
+  base = 48 + random.randint(0, 11)
+  if answer == 2:
+    chord = [base, base + 3, base + 6]
+  else:
+    chord = [base, base + 3 + answer, base + 7]
+  choice = ['minor', 'major', 'diminished']
+  session['expected'] = answer
+  return jsonify(chord=chord, choice=choice)
+
+@app.route('/_answerfirstquestion')
+def answer_first_question():
+  if not 'expected' in session:
+    return jsonify(error='problem or session cookie corrupted')
+  expected = session['expected']
+  answer = request.args.get('answer', None)
+  if answer is None:
+    return jsonify(error='expected answer')
+  # TODO: sanity check int-ity
+
+  session.pop('expected', None)
+  tooltip = ['minor', 'major', 'diminished']
+  try:
+    answer = int(answer)
+    expected = int(expected)
+    answer_tooltip = tooltip[answer]
+    expected_tooltip = tooltip[expected]
+  except (ValueError, IndexError) as e:
+    return jsonify(error='Something went wrong: %s' % e)
+  if answer == expected:
+    return jsonify(result='Yes!')
+  else:
+    return jsonify(result='No. You answered %s, %s was generated instead' %
+        (answer_tooltip, expected_tooltip)
+        )
+
+with open('secret') as secret_file:
+  app.secret_key = secret_file.readline().rstrip('\n')
 
 if __name__ == '__main__':
   app.run()
