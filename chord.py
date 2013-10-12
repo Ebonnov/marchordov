f2155f293f9718c0d21e66ad5be5dd3e935adf00
ebonnov <ebonnov@gmail.com>
2013-10-12 00:40:15 -0700

    Session 2: chords in a random key
    - Session control (and more buttons and more confusions)
    - chord_in_key session

--- chord.py
+++ chord.py
@@ -42,7 +42,6 @@ def answer_first_question():
   answer = request.args.get('answer', None)
   if answer is None:
     return jsonify(error='expected answer')
-  # TODO: sanity check int-ity
 
   session.pop('expected', None)
   tooltip = ['minor', 'major', 'diminished']
@@ -59,6 +58,118 @@ def answer_first_question():
     return jsonify(result='No. You answered %s, %s was generated instead' %
         (answer_tooltip, expected_tooltip)
         )
+
+def create_dummy_session():
+  # This is trivial.
+  return jsonify(endpoint='_generatefirstquestion')
+
+def create_inkey_session():
+  key = random.randint(0, 11)
+  major = random.randint(0, 1)
+  session['key'] = key
+  # Bad naming
+  session['majorness'] = major
+  # major: 0 2 4 5 7 9 11
+  # 0 4 7, 2 5 9, 4 7 11, 5 9 12, 7 11 14, 9 12 16, 11 14 17
+  # minor: 0 2 3 5 7 8 11?
+  # i.e. 0 3 7, 2 5 8, 3 7 10, 5 8 12, 7 11 14, 8 12 15, 11 14 18
+  # Need to 'print' a I or i here
+  base = [45 + key, 48 + key + major, 52 + key, 57 + key]
+  return jsonify(baseprint=base, endpoint='_generateinkeyquestion')
+
+inkey_chords = [
+  [
+    ([0, 3, 7], 'i'),
+    ([2, 5, 8], 'ii-'),
+    ([3, 7, 10], 'III'),
+    ([5, 8, 12], 'iv'),
+    ([7, 11, 14], 'V'),
+    ([8, 12, 15], 'VI'),
+    ([11, 14, 18], 'vii'),
+  ],[
+    ([0, 4, 7], 'I'),
+    ([2, 5, 9], 'ii'),
+    ([4, 7, 11], 'iii'),
+    ([5, 9, 12], 'IV'),
+    ([7, 11, 14], 'V'),
+    ([9, 12, 16], 'vi'),
+    ([11, 14, 17], 'vii-'),
+  ]
+]
+
+@app.route('/_generateinkeyquestion')
+def generate_inkey_question():
+  if 'session' not in session or session['session'] != 'inkey':
+    return jsonify(error='wrong session')
+  if 'key' not in session or 'majorness' not in session:
+    return jsonify(error='session corrupted')
+  chord_set = inkey_chords[session['majorness']]
+  # randint and I hate each other
+  expected = random.randint(0, len(chord_set) - 1)
+  chord = [x + 45 + session['key'] for x in chord_set[expected][0]]
+  session['expected'] = expected
+  return jsonify(chord=chord, choice=[c[1] for c in chord_set],
+    endpoint='_answerinkeyquestion')
+
+@app.route('/_answerinkeyquestion')
+def answer_inkey_question():
+  if not 'expected' in session:
+    return jsonify(error='problem or session cookie corrupted')
+  expected = session['expected']
+  answer = request.args.get('answer', None)
+  if answer is None:
+    return jsonify(error='expected answer')
+
+  session.pop('expected', None)
+  tooltip = [c[1] for c in inkey_chords[session['majorness']]]
+  try:
+    answer = int(answer)
+    expected = int(expected)
+    answer_tooltip = tooltip[answer]
+    expected_tooltip = tooltip[expected]
+  except (ValueError, IndexError) as e:
+    return jsonify(error='Something went wrong: %s' % e)
+  if answer == expected:
+    return jsonify(result='Yes!')
+  else:
+    return jsonify(result='No. You answered %s, %s was generated instead' %
+        (answer_tooltip, expected_tooltip)
+        )
+
+#   <---      what session you're in, what session available
+#   --->      start session
+#             roll dice for the session
+#   <---      key to show, set state
+#   --->      start first problem
+# Am I reinventing a wheel?
+
+@app.route('/_create_session')
+def create_session():
+  if 'session' in session:
+    return jsonify(error='fur')
+  session_name_supported = {
+    'dummy': create_dummy_session,
+    'inkey': create_inkey_session,
+  }
+  name = request.args.get('session_name', None)
+  if name not in session_name_supported:
+    return jsonify(error='furfur')
+  session['session'] = name
+  return session_name_supported[name]()
+
+@app.route('/_query_session')
+def query_session():
+  if 'session' in session:
+    return jsonify(has_session='yes', session=session['session'])
+  else:
+    # probably just use if data.session to tell anyway
+    return jsonify(has_session='no')
+
+@app.route('/_clear_session')
+def clear_session():
+  if 'session' in session:
+    del session['session']
+  return jsonify()
 
 if __name__ == '__main__':
   app.run()
